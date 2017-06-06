b5ab34264a2c9538715974aaf00ab83daed43946
Ryan Volz <rvolz@mit.edu>
2017-06-06 10:55:31 -0400

    python: Move list_drf so it always imports, check for watchdog in drf.

--- python/__init__.py
+++ python/__init__.py
@@ -2,12 +2,12 @@
 from ._version import __version__
 from .digital_metadata import *
 from .digital_rf_hdf5 import *
+from . import list_drf
+from .list_drf import lsdrf
 try:
-    from . import list_drf
     from . import mirror
     from . import ringbuffer
     from . import watchdog_drf
-    from .list_drf import lsdrf
 except ImportError:
     # if no watchdog package, these fail to import, so just ignore
     pass
--- python/drf_command.py
+++ python/drf_command.py
@@ -1,9 +1,16 @@
 from argparse import ArgumentParser
 
 from .list_drf import _build_ls_parser
-from .mirror import _build_mirror_parser
-from .ringbuffer import _build_ringbuffer_parser
-from .watchdog_drf import _build_watch_parser
+try:
+    from .mirror import _build_mirror_parser
+    from .ringbuffer import _build_ringbuffer_parser
+    from .watchdog_drf import _build_watch_parser
+except ImportError:
+    # if no watchdog package, these fail to import, so just ignore
+    _WATCHDOG = False
+else:
+    _WATCHDOG = True
+
 
 def main(args=None):
     parser = ArgumentParser(
@@ -17,9 +24,10 @@ def main(args=None):
     )
 
     _build_ls_parser(subparsers.add_parser, 'ls')
-    _build_mirror_parser(subparsers.add_parser, 'mirror')
-    _build_ringbuffer_parser(subparsers.add_parser, 'ringbuffer')
-    _build_watch_parser(subparsers.add_parser, 'watch')
+    if _WATCHDOG:
+        _build_mirror_parser(subparsers.add_parser, 'mirror')
+        _build_ringbuffer_parser(subparsers.add_parser, 'ringbuffer')
+        _build_watch_parser(subparsers.add_parser, 'watch')
 
     # parse the command line and/or function call arguments
     parsed = parser.parse_args(args)
