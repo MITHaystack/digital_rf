0598b9e0268f88162375f407a00c516211fa8777
Ryan Volz <ryan.volz@gmail.com>
2017-07-22 14:15:33 -0400

    CMake: Update digital_rfConfig.cmake to properly find HDF5 library.

--- cmake/digital_rfConfig.cmake
+++ cmake/digital_rfConfig.cmake
@@ -1,7 +1,17 @@
 include(CMakeFindDependencyMacro)
 find_dependency(HDF5 REQUIRED)
 
-find_library(HDF5_LIBRARY hdf5 PATHS ${HDF5_LIBRARY_DIRS})
+# need the dirs to explicitly find just the 'hdf5' library
+set(HDF5_LIBRARY_DIRS)
+foreach(LIB IN LISTS HDF5_LIBRARIES)
+    get_filename_component(LIBDIR ${LIB} DIRECTORY)
+    list(APPEND HDF5_LIBRARY_DIRS ${LIBDIR})
+endforeach(LIB)
+list(REMOVE_DUPLICATES HDF5_LIBRARY_DIRS)
+find_library(HDF5_LIBRARY hdf5 HINTS ${HDF5_LIBRARY_DIRS})
+# need the library directory for python build (guaranteed to exist since we
+# found HDF5 package and must have found the 'hdf5' library)
+get_filename_component(HDF5_LIBRARY_DIR ${HDF5_LIBRARY} DIRECTORY)
 add_library(digital_rf::hdf5 SHARED IMPORTED)
 set_target_properties(digital_rf::hdf5 PROPERTIES
     IMPORTED_LOCATION ${HDF5_LIBRARY}
