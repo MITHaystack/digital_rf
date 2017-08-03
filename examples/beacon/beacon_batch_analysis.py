acfc7b59d80eb0b77eab9913a19e1c98d8c0df19
John Swoboda <swoboj@mit.edu>
2017-08-03 18:50:34 -0400

    Added doppler plotting function

--- examples/beacon/beacon_batch_analysis.py
+++ examples/beacon/beacon_batch_analysis.py
@@ -20,6 +20,11 @@ import sys
 import argparse
 import subprocess
 import shutil
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+from beacon_analysis import corr_tle_rf
 def parse_command_line(str_input=None):
     """
         This will parse through the command line arguments
@@ -61,6 +66,39 @@ def parse_command_line(str_input=None):
         return parser.parse_args()
     return parser.parse_args(str_input)
 
+def plot_dops(maindir, savename,tleoff):
+    sns.set_context("notebook")
+    sns.set_style("whitegrid")
+
+    _, _, tvec, dopfit0, dopfit1, doppler0, doppler1 = corr_tle_rf(maindir, tleoff=tleoff)
+    tvec0 = tvec-tvec[0]
+    tvec_c = tvec0-tvec0[-1]/2.
+    tlim = [-150, 150]
+    flim = [-10, 10]
+
+    fig1 = plt.figure(figsize=(6, 12))
+
+    plt.subplot(211)
+    dfith, tleh = plt.plot(tvec_c, dopfit0*1e-3, 'bx', tvec_c, doppler0*1e-3, 'ro')
+
+    plt.xlabel('Time in Seconds')
+    plt.ylabel('Frequency in kHz')
+    plt.title('TLE offset: {0} s. Chan:150 MHz'.format(tleoff))
+    plt.legend([dfith, tleh], ['IQ', 'TLE'])
+    plt.xlim(tlim)
+    plt.ylim(flim)
+    plt.grid(True)
+
+    plt.subplot(212)
+    dfith, tleh = plt.plot(tvec_c, dopfit1*1e-3, 'bx', tvec_c, doppler1*1e-3, 'ro')
+    plt.xlabel('Time in Seconds')
+    plt.ylabel('Frequency in kHz')
+    plt.title('TLE offset: {0} s. Chan: 400 MHz'.format(tleoff))
+    plt.legend([dfith, tleh], ['IQ', 'TLE'])
+    plt.xlim(tlim)
+    plt.ylim(flim)
+    plt.grid(True)
+    fig1.savefig(savename)
 
 def beacon_list(input_args):
     """ """
@@ -86,6 +124,8 @@ def beacon_list(input_args):
 
             oldfig = os.path.join(newdir, 'Figures', 'chancomp.png')
             newfile = os.path.join(figsdir, curdir+'.png')
+            savename = os.path.join(figsdir, 'comp' + curdir+'.png')
+            plot_dops(rfdir, savename, curtleoff)
             if os.path.exists(oldfig):
                 shutil.copy(oldfig, newfile)
 if __name__ == '__main__':
