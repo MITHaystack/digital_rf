13cf4dd7d00a0708357cc8360047e547d6066103
Ryan Volz <ryan.volz@gmail.com>
2018-02-14 17:11:30 -0500

    c: Only link against hdf5 library and not what it linked against.

--- c/cmake/libdigital_rfConfig.cmake
+++ c/cmake/libdigital_rfConfig.cmake
@@ -1,8 +1,9 @@
 include(CMakeFindDependencyMacro)
 find_dependency(HDF5 REQUIRED)
 
-# make each of the HDF5_LIBRARIES (HDF5 and what it linked against)
-# an imported target
+# use imported targets from HDF5_LIBRARIES or take the supplied library path
+# and turn it into an imported target if it is an hdf5 library
+set(HDF5_LIB_TARGETS)
 foreach(LIB IN LISTS HDF5_LIBRARIES)
     if(TARGET ${LIB})
         list(APPEND HDF5_LIB_TARGETS ${LIB})
@@ -11,16 +12,19 @@ foreach(LIB IN LISTS HDF5_LIBRARIES)
         get_filename_component(LIBNAME ${LIB} NAME_WE)
         # strip leading lib (if it exists) to get target name
         string(REGEX REPLACE "^lib(.*)" "\\1" TGT ${LIBNAME})
-        add_library(digital_rf::${TGT} SHARED IMPORTED)
-        set_target_properties(digital_rf::${TGT} PROPERTIES
-            IMPORTED_LOCATION ${LIB}
-        )
-        if(HDF5_INCLUDE_DIRS)
-            set_property(TARGET digital_rf::${TGT} APPEND PROPERTY
-                INTERFACE_INCLUDE_DIRECTORIES ${HDF5_INCLUDE_DIRS}
+        # exclude non-hdf5 libraries (libs that HDF5 linked against)
+        if(${TGT} MATCHES ".*hdf5.*")
+            add_library(digital_rf::${TGT} SHARED IMPORTED GLOBAL)
+            set_target_properties(digital_rf::${TGT} PROPERTIES
+                IMPORTED_LOCATION ${LIB}
             )
-        endif(HDF5_INCLUDE_DIRS)
-        list(APPEND HDF5_LIB_TARGETS digital_rf::${TGT})
+            if(HDF5_INCLUDE_DIRS)
+                set_property(TARGET digital_rf::${TGT} APPEND PROPERTY
+                    INTERFACE_INCLUDE_DIRECTORIES ${HDF5_INCLUDE_DIRS}
+                )
+            endif(HDF5_INCLUDE_DIRS)
+            list(APPEND HDF5_LIB_TARGETS digital_rf::${TGT})
+        endif(${TGT} MATCHES ".*hdf5.*")
     endif(TARGET ${LIB})
 endforeach(LIB)
 
