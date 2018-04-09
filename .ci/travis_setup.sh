7f03da2d6f94008a48c7cc996daf2efde1de8a3b
Ryan Volz <rvolz@mit.edu>
2018-04-09 18:23:40 -0400

    ci: Fix Travis build/test for all setups, scripts -> yaml commands.

--- .ci/travis_setup.sh
+++ .ci/travis_setup.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
+set -e
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    # install non-python build requirements
-    sudo apt-get update -qq
-    sudo apt-get install -qq cmake libhdf5-dev
+    function setup_environment {
+        # install non-python build requirements
+        sudo apt-get update -qq
+        sudo apt-get install -qq cmake libhdf5-dev
+        python -m pip install -U pip
+    }
 else
-    # because osx image lacks python support, make virtualenv manually
-    git clone https://github.com/matthew-brett/multibuild
-    source multibuild/osx_utils.sh
-    get_macpython_environment $PYTHON_VERSION venv
-    # have to uninstall oclint to be able to install gcc (required by hdf5)
-    brew cask uninstall oclint
-    # install non-python build requirements
-    brew install hdf5
+    function setup_environment {
+        # because osx image lacks python support, make virtualenv manually
+        git clone https://github.com/matthew-brett/multibuild --depth 1
+        source multibuild/osx_utils.sh
+        get_macpython_environment $PYTHON_VERSION venv
+        source venv/bin/activate
+        # have to uninstall oclint to be able to install gcc (required by hdf5)
+        brew cask uninstall oclint || true
+        # install non-python build requirements
+        export HOMEBREW_NO_AUTO_UPDATE=1
+        brew install hdf5
+        python -m pip install -U pip
+    }
 fi
 
-# install python build requirements
-python -m pip install -U pip
-python -m pip install -U -r python/dev_requirements.txt
