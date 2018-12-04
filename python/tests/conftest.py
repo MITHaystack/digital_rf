5632b4f3788f5617cfe9313a971d196b229b533e
Ryan Volz <rvolz@mit.edu>
2018-12-04 12:01:47 -0500

    tests: Do not use deprecated get_marker(), dropped in pytest 4.0.
    Using the replacement iter_markers() requires pytest >= 3.6.

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -12,14 +12,19 @@ def pytest_collection_modifyitems(items):
     selected_items = []
 
     for item in items:
-        firstonly = item.get_marker('firstonly')
-        if firstonly is not None:
+        for firstonly in item.iter_markers('firstonly'):
             for param in firstonly.args:
+                # check if specified param is not first and skip
                 idx = item.callspec.indices.get(param, None)
                 if idx is not None and idx > 0:
                     break
             else:
-                selected_items.append(item)
+                # if item is not skipped on the basis of this set of params,
+                # move on to next mark in loop
+                continue
+            # if we broke out of the previous for loop and are skipping the
+            # item, we have to break out of this loop too
+            break
         else:
             selected_items.append(item)
 
