6f2068d054318e234d241be441c10842f949c2a3
Ryan Volz <rvolz@mit.edu>
2024-08-15 15:39:55 -0400

    Update conda recipe to allow building without git

--- recipes/conda/bld.bat
+++ recipes/conda/bld.bat
@@ -5,19 +5,20 @@ mkdir build
 cd build
 
 :: configure
-cmake -G "NMake Makefiles" ^
-      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-      -DDRF_DATA_PREFIX_PYTHON:PATH="%LIBRARY_PREFIX%" ^
+cmake -G "Ninja" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+      -DDRF_DATA_PREFIX_PYTHON="%LIBRARY_PREFIX%" ^
       -DPython_FIND_REGISTRY=NEVER ^
-      -DPython_ROOT_DIR:PATH="%PREFIX%" ^
+      -DPython_ROOT_DIR="%PREFIX%" ^
       ..
 if errorlevel 1 exit 1
 
 :: build
-cmake --build .
+cmake --build . --config Release
 if errorlevel 1 exit 1
 
 :: install
-cmake --build . --target install
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
